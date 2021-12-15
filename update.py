1025ccf3da6349cde395268d02ab84e372241ff4
Badnam Aashiq <92842718+badboy7310@users.noreply.github.com>
2021-12-16 00:02:56 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except TypeError:
-    UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
+    UPSTREAM_REPO = "https://github.com/badboy7310/GDTOT"
 
 if os.path.exists('.git'):
     subprocess.run(["rm", "-rf", ".git"])
